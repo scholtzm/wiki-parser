6929beb5386bed87d59ca0a3fdb789b49300ad93
Michael Scholtz <michael.scholtz@outlook.com>
2014-10-31 20:36:55 +0100

    Added Lucene search.

---
+++ src/sk/scholtz/wikiparser/LuceneSearch.java
@@ -0,0 +1,95 @@
+package sk.scholtz.wikiparser;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.IndexableField;
+import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopScoreDocCollector;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.RAMDirectory;
+import org.apache.lucene.util.Version;
+
+public class LuceneSearch {
+    private StandardAnalyzer analyzer;
+    private Directory index;
+
+    private final Version VERSION = Version.LUCENE_4_10_1;
+    private final int MAX_HITS = 100;
+
+    public LuceneSearch() {
+        this.analyzer = new StandardAnalyzer();
+    }
+
+    /**
+     * This will create complete index of our XML dump
+     * @param wikiRecords Result of XmlParser.parseDump method
+     * @throws IOException
+     * @throws ParseException
+     */
+    public void createIndex(HashMap<String, Record> wikiRecords) throws IOException, ParseException {
+        // RAMDirectory seems to be ideal for small indexes
+        index = new RAMDirectory();
+        IndexWriterConfig config = new IndexWriterConfig(VERSION, analyzer);
+        IndexWriter indexWriter = new IndexWriter(index, config);
+
+        for(Entry<String, Record> record : wikiRecords.entrySet()) {
+            Document document = new Document();
+
+            document.add(new TextField("title", record.getValue().getTitle(), Field.Store.YES));
+            for(String alt : record.getValue().getAlternatives())
+                document.add(new TextField("alt", alt, Field.Store.YES));
+
+            indexWriter.addDocument(document);
+        }
+
+        indexWriter.close();
+    }
+
+    /**
+     * Search our index
+     * @param query Search query
+     * @throws IOException
+     * @throws ParseException
+     */
+    public void search(String query) throws IOException, ParseException {
+        // Create multi-field query
+        String[] fields = new String[] {"title", "alt"};
+        Query multiFieldQuery = new MultiFieldQueryParser(fields, analyzer).parse(query);
+
+        // Start search
+        IndexReader reader = DirectoryReader.open(index);
+        IndexSearcher searcher = new IndexSearcher(reader);
+        TopScoreDocCollector collector = TopScoreDocCollector.create(MAX_HITS, true);
+        searcher.search(multiFieldQuery, collector);
+        ScoreDoc[] results = collector.topDocs().scoreDocs;
+
+        // Dump results
+        System.out.println("Found " + results.length + " search results.");
+        for (int i = 0; i < results.length; i++) {
+            int resultId = results[i].doc;
+            Document d = searcher.doc(resultId);
+            System.out.println((i + 1) + ". " + d.get("title"));
+
+            for(IndexableField inf : d.getFields("alt")) {
+                System.out.println("\t" + inf.stringValue());
+            }
+        }
+
+        // Do not forget to close the reader
+        reader.close();
+    }
+}
--- src/sk/scholtz/wikiparser/Main.java
+++ src/sk/scholtz/wikiparser/Main.java
@@ -1,5 +1,6 @@
 package sk.scholtz.wikiparser;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -18,55 +19,116 @@ public class Main {
         // Timer variable
         long t1;
 
-        if (args.length < 1) {
-            System.out.println(TITLE + " - " + AUTHOR + " - " + YEAR);
-            System.out.println("Usage: java -jar wikiparser.jar <input>");
-            System.out.println("    input : input file in valid XML format");
-
+        if (args.length < 2 || args.length > 50) {
+            printHelp();
             return;
         }
 
-        String inputFile = args[0];
+        OperationMode opMode;
+
+        if(args[0].equals("-W")) {
+            opMode = OperationMode.WIKIPEDIA;
+        } else if(args[0].equals("-L")) {
+            opMode = OperationMode.WIKIPARSER;
+        } else {
+            printHelp();
+            return;
+        }
+
+        String inputFile = args[1];
         String outputFile = inputFile + "-output.xml";
+        String query = args.length >= 3 ? args[2] : "";
 
         System.out.println("Starting " + TITLE + "...");
         System.out.println("Working Directory: " + System.getProperty("user.dir"));
+        System.out.println("Mode: " + opMode.toString());
+
+        if(opMode == OperationMode.WIKIPARSER)
+            System.out.println("Query: " + query);
+
         System.out.println("Input file: " + inputFile);
-        System.out.println("Output file: " + outputFile);
+
+        if(opMode == OperationMode.WIKIPEDIA)
+            System.out.println("Output file: " + outputFile);
 
         // Setup XmlParser
         XmlParser xmlParser = new XmlParser();
 
-        // Parse inputFile
-        t1 = System.currentTimeMillis();
-        System.out.println("Parsing data ...");
+     // Parsing and processing Wikipedia dumps
+        if(opMode == OperationMode.WIKIPEDIA) {
+            // Parse inputFile
+            t1 = System.currentTimeMillis();
+            System.out.println("Parsing data ...");
 
-        ArrayList<Page> wikiPages = xmlParser.parseWiki(inputFile);
+            ArrayList<Page> wikiPages = xmlParser.parseWiki(inputFile);
 
-        System.out.println(String.format("Parsing took: %.3f seconds", ((double)(System.currentTimeMillis() - t1) / 1000)));
+            System.out.println(String.format("Parsing took: %.3f seconds", ((double)(System.currentTimeMillis() - t1) / 1000)));
 
-        // Setup our Wiki processor
-        Processor processor = new Processor(wikiPages);
+            // Setup our Wiki processor
+            Processor processor = new Processor(wikiPages);
 
-        // Process output of our XmlParser
-        t1 = System.currentTimeMillis();
-        System.out.println("Processing data ...");
+            // Process output of our XmlParser
+            t1 = System.currentTimeMillis();
+            System.out.println("Processing data ...");
 
-        HashMap<String, Record> wikiRecords = processor.DoWork();
+            HashMap<String, Record> wikiRecords = processor.DoWork();
 
-        System.out.println(String.format("Processing took: %.3f seconds", ((double)(System.currentTimeMillis() - t1) / 1000)));
+            System.out.println(String.format("Processing took: %.3f seconds", ((double)(System.currentTimeMillis() - t1) / 1000)));
 
-        // Write output XML
-        t1 = System.currentTimeMillis();
-        System.out.println("Writing output file ...");
+            // Write output XML
+            t1 = System.currentTimeMillis();
+            System.out.println("Writing output file ...");
 
-        xmlParser.write(outputFile, wikiRecords);
+            xmlParser.write(outputFile, wikiRecords);
 
-        System.out.println(String.format("Writing took: %.3f seconds", ((double)(System.currentTimeMillis() - t1) / 1000)));
+            System.out.println(String.format("Writing took: %.3f seconds", ((double)(System.currentTimeMillis() - t1) / 1000)));
 
-        // Stats
-        System.out.println("Found " + wikiPages.size() + " pages.");
-        System.out.println("Found " + wikiRecords.size() + " unique pages.");
+            // Stats
+            System.out.println("Found " + wikiPages.size() + " pages.");
+            System.out.println("Found " + wikiRecords.size() + " unique pages.");
+
+        // Parsing, indexing and searching Wikiparser dumps
+        } else if(opMode == OperationMode.WIKIPARSER) {
+            // Process XML and create index
+            t1 = System.currentTimeMillis();
+            System.out.println("Processing XML dump and creating indexes ...");
+
+            HashMap<String, Record> wikiRecords = xmlParser.parseDump(new File(inputFile).getAbsolutePath());
+
+            LuceneSearch lucene = new LuceneSearch();
+            try {
+                lucene.createIndex(wikiRecords);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            System.out.println(String.format("Processing took: %.3f seconds", ((double)(System.currentTimeMillis() - t1) / 1000)));
+
+            // Commence search
+            t1 = System.currentTimeMillis();
+            System.out.println("Commencing search ...");
+
+            try {
+                lucene.search(query);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            System.out.println(String.format("Searching took: %.3f seconds", ((double)(System.currentTimeMillis() - t1) / 1000)));
+        }
     }
 
+    public static void printHelp() {
+        System.out.println(TITLE + " - " + AUTHOR + " - " + YEAR);
+        System.out.println("Usage: java -jar wikiparser.jar [-W input|-L input query]");
+        System.out.println("    -W : parse and process valid Wikipedia dump");
+        System.out.println("    -L : index and search valid Wikiparser dump");
+        System.out.println("    input : input file in valid XML format");
+        System.out.println("    query : Lucene query");
+    }
+
+    public enum OperationMode {
+        WIKIPEDIA,
+        WIKIPARSER
+    }
 }
